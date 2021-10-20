5fce6128ea68b0ef35c8ead4a16950c9850f12a6
Truls Henrik <trulshj@stud.ntnu.no>
2021-10-20 19:43:18 +0200

    Fixes after feedback

--- oving1/oppgave2.cpp
+++ oving1/oppgave2.cpp
@@ -7,25 +7,25 @@ using namespace std;
 void read_temperatures(double temperatures[], int length);
 
 int main() {
-  const int length = 5;
-  double temperatures[length];
+    const int length = 5;
+    double temperatures[length];
 
-  read_temperatures(temperatures, length);
+    read_temperatures(temperatures, length);
 }
 
 void read_temperatures(double temperatures[], int length) {
-  const char filename[] = "/Users/trulshj/school/INFT2503/1/temperaturfil.dat";
-  ifstream file;
-  file.open(filename);
-  if (!file) {
-    cout << "Feil ved åpning av temperaturfilen." << endl;
-    exit(EXIT_FAILURE);
-  }
+    const char filename[] = "/Users/trulshj/school/INFT2503/1/temperaturfil.dat";
+    ifstream file;
+    file.open(filename);
+    if (!file) {
+        cout << "Feil ved åpning av temperaturfilen." << endl;
+        exit(EXIT_FAILURE);
+    }
 
-  double temperature;
-  int i = 0;
-  while (file >> temperature and i < length) {
-    temperatures[i] = temperature;
-  }
-  file.close();
+    double temperature;
+    int i = 0;
+    while (file >> temperature && i < length) {
+        temperatures[i] = temperature;
+    }
+    file.close();
 }
